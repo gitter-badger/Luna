a2d66dd369dd24e532d8280ab06193a7612544f4
Yannick <contact@studio384.be>
2015-06-04 22:05:17 +0200

    #1297 Step 1: get the database type's database layer

---
+++ include/ntdblayer/get_db.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Copyright (C) 2013-2015 Luna
+ * License: http://opensource.org/licenses/MIT MIT
+ */
+
+// Load the database layer we need
+switch ($db_type) {
+	default:
+		error('We can\'t find a \''.$db_type.'\'-based database. Please check the settings given in config.php.', __FILE__, __LINE__);
+		break;
+}
+
+// Connect to the database
+$db = new DBConnect($db_host, $db_username, $db_password, $db_name, $db_prefix, $p_connect);
