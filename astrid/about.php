dc7233aae07ebdc4ba7bec400661224d1b77bd21
Yannick <contact@studio384.be>
2015-06-13 21:30:31 +0200

    #1299 Include the About page

---
+++ astrid/about.php
@@ -0,0 +1,63 @@
+<?php include('include/header.php') ?>
+<div class="row">
+	<div class="col-sm-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Navigation</h3>
+			</div>
+			<div class="list-group">
+				<a href="#astrid" class="list-group-item"><span class="fa fa-fw fa-dashboard"></span> Astrid</a>
+				<a href="#database" class="list-group-item"><span class="fa fa-fw fa-database"></span> Database</a>
+				<a href="#others" class="list-group-item">Other improvements and notes</a>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-9">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">About Luna 2.0 Denim</h3>
+			</div>
+			<div class="panel-body panel-about">
+				<a id="astrid"></a><h3><span class="fa fa-fw fa-dashboard"></span> Astrid</h3>
+				<div class="row">
+					<div class="col-sm-6">
+						<h4>Rebuild from the ground up</h4>
+						<p>Luna 2.0 comes with a whole new Backstage that has been rebuild from the ground up to suit your needs.</p>
+					</div>
+				</div>
+				<a id="database"></a><h3><span class="fa fa-fw fa-database"></span> Database</h3>
+				<div class="row">
+					<div class="col-sm-6">
+						<h4>Database layer</h4>
+						<p>The database layers have been rewritten to include new functions that make our codebase smaller and other performance improvements and APIs for developers.</p>
+					</div>
+					<div class="col-sm-6">
+						<h4>SQLite 3</h4>
+						<p>Luna finally supports SQLite 3 as of version 2.0. If you have a system with SQLite 3-support and you want Luna on that system, here's your time!</p>
+					</div>
+				</div>
+				<a id="others"></a><h3>Other improvements and notes</h3>
+				<div class="row">
+					<div class="col-sm-12">
+						<h4>And a lot of other small changes</h4>
+						<p>However, since Luna 1.1 has its focus on refinements, there are also a lot of other small improvements all over the board (no pun intended). This includes better performance, more developer options, small visual changes and fixes, and so much more.</p>
+					</div>
+					<div class="col-sm-6">
+						<h4>Packages</h4>
+						<p><b>Core</b> has been updated from version 1.1.4719 to 2.0.0.39..</p>
+					</div>
+					<div class="col-sm-6">
+						<h4>Bug fixes</h4>
+						<p>0 bugs have been fixed.</p>
+						<h4>Security fixes</h4>
+						<p>0 security issues has been fixed.</p>
+					</div>
+				</div>
+			</div>
+			<div class="panel-footer">
+				<p>Luna is developed by the <a href="http://getluna.org/">Luna Group</a>. Copyright 2013-2015. Released under the MIT license.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include('include/footer.php') ?>
