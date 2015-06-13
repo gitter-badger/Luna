3c08d836b56c1579824573c144a13b30854b3391
Yannick <contact@studio384.be>
2015-06-13 21:23:21 +0200

    #1299 Update header and footer

--- astrid/include/header.php
+++ astrid/include/header.php
@@ -1,3 +1,6 @@
+<?php
+include_once('include/version.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -19,7 +22,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="../"><i class="fa fa-fw fa-arrow-left"></i></a>
+					<a class="navbar-brand visible-xs-inline" href="../">Backstage</a>
 				</div>
 				<div class="collapse navbar-collapse" id="main-nav">
 					<ul class="nav navbar-nav">
@@ -31,6 +34,7 @@
 						<li><a href="#"><i class="fa fa-fw fa-plus"></i> Extras</a></li>
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#"><i class="fa fa-fw fa-circle-o"></i><span class="visible-xs-inline"> Notifications</span></a></li>
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-fw fa-angle-down"></i><span class="visible-xs-inline"> Astrid</span></a>
 							<ul class="dropdown-menu" role="menu">
@@ -65,4 +69,3 @@
 		</div>
 		<div class="container">
 			<div class="row">
-				<div class="row">
